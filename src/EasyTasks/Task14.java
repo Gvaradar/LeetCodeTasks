decd4d4d748ad0dcc6505d99b90099c63d263615
Djon <lioni767@yandex.ru>
2023-11-03 20:43:29 +0300

    Initial commit

---
+++ src/EasyTasks/Task14.java
@@ -0,0 +1,14 @@
+package EasyTasks;
+
+public class Task14 {
+    public String longestCommonPrefix(String[] strs) {
+        boolean notTheSame = false;
+        int countLetter = 0;
+        while (!notTheSame){
+        
+        countLetter++;
+        }
+
+        return "";
+    }
+}
